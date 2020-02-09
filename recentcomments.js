d8b48d27a84126a0992b3fe5277b4e8f3750ae01
sandboxandgithub <60837150+sandboxandgithub@users.noreply.github.com>
2020-02-09 18:36:09 +0900

    Update recentcomments.js

--- recentcomments.js
+++ recentcomments.js
@@ -1,5 +1,7 @@
 //<![CDATA[
 
+var a_rc = 5; var m_rc = false; var n_rc = false; var o_rc = 100;
+
 function showrecentcomments(json){
     for(var i=0;i<a_rc;i++){
         var b_rc=json.feed.entry[i];
